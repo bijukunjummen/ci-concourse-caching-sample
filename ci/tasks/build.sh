5f0164f3391529b51b23f8a50e5dd10714f09cd6
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-08-14 16:35:15 -0700

    Fixes path to project

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 
+export ROOT_FOLDER=$( pwd )
+export REPO=repo
+
+
 __SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 [[ -f "${__SCRIPTS_DIR}/functions.sh" ]] && source "${__SCRIPTS_DIR}/functions.sh" || \
     echo "No functions.sh found"
 
+
+cd ${ROOT_FOLDER}/${REPO}
 build "${PROJECT_TYPE}"
