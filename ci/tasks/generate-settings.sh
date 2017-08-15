e6817f55716376191e6ea9876f014cea2abef41c
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-08-14 17:12:11 -0700

    Fixed m2 paths

--- ci/tasks/generate-settings.sh
+++ ci/tasks/generate-settings.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-mkdir -p ${HOME}/.m2
+M2_HOME=${HOME}/.m2
+mkdir -p ${M2_HOME}
 
 M2_LOCAL_REPO="${ROOT_FOLDER}/.m2"
 
 mkdir -p "${M2_LOCAL_REPO}/repository"
 
-cat > ${M2_LOCAL_REPO}/settings.xml <<EOF
+cat > ${M2_HOME}/settings.xml <<EOF
 
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                           https://maven.apache.org/xsd/settings-1.0.0.xsd">
-      <localRepository>${M2_LOCAL_REPO}</localRepository>
+      <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
 </settings>
 
 EOF
 echo "settings.xml updated with local repository to : " ${M2_LOCAL_REPO}/repository
-
-cat ${M2_LOCAL_REPO}/settings.xml
 
 export GRADLE_USER_HOME="${ROOT_FOLDER}/.gradle"
 mkdir -p ${GRADLE_USER_HOME}
