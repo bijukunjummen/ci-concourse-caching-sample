86d47b17c0a0eb8fb1347ee886fa53f236579b3f
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2018-06-08 11:11:21 -0700

    User prompts to set pipeline

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -4,4 +4,4 @@ ALIAS=${1:-myconcourse}
 
 PIPELINE_NAME="ci-concourse-caching-sample"
 
-echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c ci/pipeline.yml -l "ci/credentials.yml"
+fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c ci/pipeline.yml -l "ci/credentials.yml"
